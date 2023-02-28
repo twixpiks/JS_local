07de1f7da23134bda4317f68a830089d45ed1c64
Ivan <johnmalkov933@gmail.com>
2023-02-28 23:48:49 +0400

    dsadsa

--- js/script.js
+++ js/script.js
@@ -1,39 +1,123 @@
+
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
 'use strict';
-const box = document.getElementById('box');
-const btns = document.getElementsByTagName('button');
-const circles = document.getElementsByClassName('circle');
-const hearts = document.querySelectorAll('.heart');
-const wrapper = document.querySelector('.wrapper');
-const oneheart = document.querySelector('.heart');
-console.log(oneheart)
-const oneheart2 = document.querySelector('.heart');
-//box.style.backgroundColor = 'blue';
-//box.style.width = '500px'
-btns[1].style.borderRadius = '100%';
-console.log(circles)
 
-box.style.cssText = 'background-color: green; width: 500px'
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-hearts.forEach(item => {
-    item.style.backgroundColor = 'green'
-})
 
-const div = document.createElement('div');
-div.classList.add('black');
-//document.body.append(div)
 
-wrapper.append(div);
 
-//wrapper.appendChild(div);
-//wrapper.prepend(div);
-//hearts[0].before(div);
 
-//wrapper.insertBefore(div, hearts[0])
-//circles[0].remove();
-//circles[0].replaceWith(div);
-//hearts[1].replaceWith(div)
-//circles[0].replaceWith(div);
-div.innerHTML = '<h1>Hello,World!</h1>';
-//div.textContent = 'hello!';
 
-div.insertAdjacentHTML('afterend','<h2>Header 2</h2>')
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
+
+
+
+
+
+////////mycode
+let button = document.querySelector('button');
+button.addEventListener('click', getN,);
+
+
+function getN(event) {
+    event.preventDefault();
+  let getNum = document.getElementById('qwe').value;
+  let  isFavourite = document.querySelector('[type="checkbox"]')
+  if (isFavourite.checked){
+    console.log('Добавляем любимый фильм')
+
+
+    
+
+
+  }
+
+
+  if (getNum.length >21){
+    getNum = getNum.slice(0,21) + '...'
+  };
+
+  movieDB.movies.push(getNum);
+  movieList.innerHTML = "";
+  movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+}
+
+let trashBtn = document.querySelectorAll('.delete');
+
+trashBtn.forEach((element,index) => {
+    console.log(element);
+    element.addEventListener('click', () => {
+        //let filmDiv = document.element.previousElementSibling
+        document.querySelectorAll('.promo__interactive-item')[index].remove();
+    //movieDB.movies.splice(movieDB.movies.indexOf())    
+    }
+        )
+    
+});
+
+
+
+
+
+
+///////mycodeEnd
+
